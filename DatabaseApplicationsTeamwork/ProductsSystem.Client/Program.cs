96b23792fe312a8287d874779d11aeefd1ef9ec3
Yana Slavcheva <yana.slavcheva@gmail.com>
2015-03-09 17:51:07 +0200

    Some refactoring of entry points

---
+++ DatabaseApplicationsTeamwork/ProductsSystem.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductsSystem.Client
+{
+    using ProductsSystem.Data.Contexts;
+    using ProductsSystem.Data.Data;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ProductsSystemDbContext();
+
+            // Execute the following method if do not have the
+            // database in sql server
+            // It will add sample data automatically
+            // Configuration.InitializeDatabase(context);
+            var data = ProductsSystemData.GetInstance(context);
+        }
+    }
+}
---
+++ DatabaseApplicationsTeamwork/XMLExporter/EntryPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLExporter
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            WriteXml();
+        }
+
+        public static void WriteXml()
+        {
+            var overview = new Book("Serialization Overview");
+            var writer = new System.Xml.Serialization.XmlSerializer(typeof(Book));
+
+            var file = new System.IO.StreamWriter(
+                @"c:\temp\SerializationOverview.xml");
+            writer.Serialize(file, overview);
+            file.Close();
+        }
+
+
+        public class Book
+        {
+            public Book(string title)
+            {
+                this.Title = title;
+            }
+
+            public string Title { get; set; }
+        }
+    }
+}

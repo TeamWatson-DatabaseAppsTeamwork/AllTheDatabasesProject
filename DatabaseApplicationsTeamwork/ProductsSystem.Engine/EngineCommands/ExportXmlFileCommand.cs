91759efae4f41161920a44311fdca1d335132cea
Yana Slavcheva <yana.slavcheva@gmail.com>
2015-03-11 16:49:12 +0200

    renamed files

---
+++ DatabaseApplicationsTeamwork/ProductsSystem.Engine/EngineCommands/ExportXmlFileCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace ProductsSystem.Engine.EngineCommands
+{
+    using System;
+
+    public class ExportXMLFileCommand : IEngineCommand
+    {
+
+        public System.Collections.IList Arguments
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Execute(Data.Data.IProductsSystemData data)
+        {
+            Console.WriteLine("Exporting XML...");
+            return "";
+        }
+
+        public void RecieveArguments(string[] rawArguments)
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}

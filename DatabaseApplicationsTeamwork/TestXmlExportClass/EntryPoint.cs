6477ce1dc8e3cc9fd17c94d29d5525d83a71f01d
Yana Slavcheva <yana.slavcheva@gmail.com>
2015-03-11 17:03:03 +0200

    added simple test XML export - working

---
+++ DatabaseApplicationsTeamwork/TestXmlExportClass/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿namespace TestXmlExportClass
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            WriteXml();
+        }
+
+        public static void WriteXml()
+        {
+            var overview = new Book("Serialization Overview");
+            var writer = new System.Xml.Serialization.XmlSerializer(typeof(Book));
+
+            var file = new System.IO.StreamWriter(
+                @"c:\temp\SerializationOverview.xml");
+            writer.Serialize(file, overview);
+            file.Close();
+        }
+
+
+        public class Book
+        {
+            public Book()
+            { 
+            }
+
+            public Book(string title)
+            {
+                this.Title = title;
+            }
+
+            public string Title { get; set; }
+        }
+    }
+}

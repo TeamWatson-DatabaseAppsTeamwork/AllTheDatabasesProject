2d9db28fa2121e1cfa0e6fe7702d1a821ce2c6ad
Yana Slavcheva <yana.slavcheva@gmail.com>
2015-03-14 22:58:54 +0200

    added missing file SalesSummary.cs

---
+++ DatabaseApplicationsTeamwork/ProductsSystem.DataTransferObjects/SalesSummary.cs
@@ -0,0 +1,26 @@
+﻿namespace ProductsSystem.DataTransferObjects
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [XmlType(TypeName = "summary")]
+    public class SalesSummary
+    {
+        public SalesSummary()
+        {
+        }
+
+        public SalesSummary(DateTime date, decimal totalSum)
+        {
+            this.Date = date;
+            this.TotalSum = totalSum;
+        }
+
+        [XmlAttribute("date")]
+        public DateTime Date { get; set; }
+
+
+        [XmlAttribute("total-sum")]
+        public decimal TotalSum { get; set; }
+    }
+}

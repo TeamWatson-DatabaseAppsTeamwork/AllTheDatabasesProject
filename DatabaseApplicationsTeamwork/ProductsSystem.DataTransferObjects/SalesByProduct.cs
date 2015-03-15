00332a07ccef04423eebb3b05603fc507be90324
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-03-15 20:37:33 +0200

    added files for JSON functionality

---
+++ DatabaseApplicationsTeamwork/ProductsSystem.DataTransferObjects/SalesByProduct.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductsSystem.DataTransferObjects
+{
+    using Newtonsoft.Json;
+
+    public class SalesByProduct
+    {
+        [JsonProperty(PropertyName = "product-id")]
+        public int ProductId { get; set; }
+
+        [JsonProperty(PropertyName = "product-name")]
+        public string ProductName { get; set; }
+
+        [JsonProperty(PropertyName = "vendor-name")]
+        public string VendorName { get; set; }
+
+        [JsonProperty(PropertyName = "total-quantity-sold")]
+        public int TotalQuantitySold { get; set; }
+
+        [JsonProperty(PropertyName = "total-incomes")]
+        public decimal TotalIncomes { get; set; }
+    }
+}

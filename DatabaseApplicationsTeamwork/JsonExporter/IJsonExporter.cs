9074b3b330ab8310ff333fdcbac12f4f5a7baa0f
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-03-15 20:38:25 +0200

    added files for JSON functionality

---
+++ DatabaseApplicationsTeamwork/JsonExporter/IJsonExporter.cs
@@ -0,0 +1,19 @@
+﻿namespace JsonExporter
+{
+    using System.Collections.Generic;
+
+    using ProductsSystem.DataTransferObjects;
+
+    public interface IJsonExporter
+    {
+        string FileFolderPath { get; }
+
+        string FileName { get; }
+
+        void Export(IList<SalesByProduct> aggregatedSalesData);
+
+        void SetDefaultFileFolder(string fileFolderPath);
+
+        void SetFileName(string fileName);
+    }
+}
